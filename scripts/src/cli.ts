835449308cea8807dc2e88df82b4cb8fa8b894fb
Loïc Mahieu <mahieuloic@gmail.com>
2018-10-01 21:21:06 +0200

    style

--- scripts/src/cli.ts
+++ scripts/src/cli.ts
@@ -43,80 +43,77 @@ yargs
     type: "string",
   })
 
-  .command("backup", "Run backup", (cmdArgs: yargs.Argv) => {
-    return cmdArgs
-      .option("mysqlDataDirectory", {
-        default: process.env.MYSQL_DATA_DIRECTORY || "/var/lib/mysql",
-        type: "string",
-      })
-      .option("mysqlHost", {
-        default: process.env.MYSQL_HOST || "127.0.0.1",
-        type: "string",
-      })
-      .option("mysqlPassword", {
-        default: process.env.MYSQL_PASSWORD || process.env.MYSQL_ROOT_PASSWORD || "",
-        type: "string",
-      })
-      .option("mysqlPort", {
-        default: process.env.MYSQL_PORT || 3306,
-        type: "number",
-      })
-      .option("mysqlUser", {
-        default: process.env.MYSQL_USER || "root",
-        type: "string",
-      })
+  .command("backup", "Run backup", (cmdArgs: yargs.Argv) => cmdArgs
+    .option("mysqlDataDirectory", {
+      default: process.env.MYSQL_DATA_DIRECTORY || "/var/lib/mysql",
+      type: "string",
+    })
+    .option("mysqlHost", {
+      default: process.env.MYSQL_HOST || "127.0.0.1",
+      type: "string",
+    })
+    .option("mysqlPassword", {
+      default: process.env.MYSQL_PASSWORD || process.env.MYSQL_ROOT_PASSWORD || "",
+      type: "string",
+    })
+    .option("mysqlPort", {
+      default: process.env.MYSQL_PORT || 3306,
+      type: "number",
+    })
+    .option("mysqlUser", {
+      default: process.env.MYSQL_USER || "root",
+      type: "string",
+    })
 
-      .option("backupDirectory", {
-        default: process.env.BACKUP_DIRECTORY || "/backup",
-        type: "string",
-      })
-      .option("backupMaxAge", {
-        default: 2,
-        describe: "In day",
-        type: "number",
-      });
-  }, createJob(runBackup))
+    .option("backupDirectory", {
+      default: process.env.BACKUP_DIRECTORY || "/backup",
+      type: "string",
+    })
+    .option("backupMaxAge", {
+      default: 2,
+      describe: "In day",
+      type: "number",
+    })
+  , createJob(runBackup))
 
-  .command("prepare", "Run prepare", (cmdArgs: yargs.Argv) => {
-    return cmdArgs
-      .option("tempDirectory", {
-        default: () => tempy.directory(),
-        type: "string",
-      })
+  .command("prepare", "Run prepare", (cmdArgs: yargs.Argv) => cmdArgs
+    .option("tempDirectory", {
+      default: () => tempy.directory(),
+      type: "string",
+    })
 
-      .option("gcloudTargetPath", {
-        default: process.env.GCLOUD_TARGET_PATH || "",
-        required: true,
-        type: "string",
-      });
-  }, createJob(runPrepare))
+    .option("gcloudTargetPath", {
+      default: process.env.GCLOUD_TARGET_PATH || "",
+      required: true,
+      type: "string",
+    })
+  , createJob(runPrepare))
 
-  .command("extract", "Run extract", (cmdArgs: yargs.Argv) => {
-    return cmdArgs
-      .option("mysqlDataDirectory", {
-        default: process.env.MYSQL_DATA_DIRECTORY || "/var/lib/mysql",
-        type: "string",
-      })
-      .option("mysqlPassword", {
-        default: process.env.MYSQL_PASSWORD || process.env.MYSQL_ROOT_PASSWORD || "",
-        type: "string",
-      })
-      .option("mysqlUser", {
-        default: process.env.MYSQL_USER || "root",
-        type: "string",
-      })
+  .command("extract", "Run extract", (cmdArgs: yargs.Argv) => cmdArgs
+    .option("mysqlDataDirectory", {
+      default: process.env.MYSQL_DATA_DIRECTORY || "/var/lib/mysql",
+      type: "string",
+    })
+    .option("mysqlPassword", {
+      default: process.env.MYSQL_PASSWORD || process.env.MYSQL_ROOT_PASSWORD || "",
+      type: "string",
+    })
+    .option("mysqlUser", {
+      default: process.env.MYSQL_USER || "root",
+      type: "string",
+    })
 
-      .option("tempDirectory", {
-        default: () => tempy.directory(),
-        type: "string",
-      })
+    .option("tempDirectory", {
+      default: () => tempy.directory(),
+      type: "string",
+    })
 
-      .option("gcloudTargetPath", {
-        default: process.env.GCLOUD_TARGET_PATH || "",
-        required: true,
-        type: "string",
-      });
-  }, createJob(runExtract))
+    .option("gcloudTargetPath", {
+      default: process.env.GCLOUD_TARGET_PATH || "",
+      required: true,
+      type: "string",
+    })
+  , createJob(runExtract))
 
   .help()
   .demandCommand(1, "You need at least one command")
