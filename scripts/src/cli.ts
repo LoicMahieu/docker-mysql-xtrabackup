282a01d83539210aff23e100ea77bd359b5a9661
Loïc Mahieu <mahieuloic@gmail.com>
2018-10-01 10:20:22 +0200

    cli options

--- scripts/src/cli.ts
+++ scripts/src/cli.ts
@@ -3,6 +3,20 @@ import * as yargs from "yargs";
 import { run } from ".";
 
 const argv =  yargs
+  .option("mysqlUser", {})
+  .option("mysqlRootPassword", {})
+  .option("mysqlHost", {})
+  .option("mysqlPort", {})
+  .option("mysqlDataDirectory", {})
+
+  .option("gcloudServiceAccountKey?", {})
+  .option("gcloudServiceAccountFile?", {})
+  .option("gcloudBackupPath", {})
+
+  .option("backupDirectory", {})
+  .option("backupMaxAge", {})
   .argv;
+
+console.log(argv);
 
 run(argv);
