4b19909b49810e884603254b980c5fa34d439b17
Loïc Mahieu <mahieuloic@gmail.com>
2018-10-03 22:41:27 +0200

    options: key or file is not mandatory anymore

--- src/base-options.ts
+++ src/base-options.ts
@@ -9,7 +9,4 @@ export function validateOptions(options: IBaseOptions) {
   if (!options.gcloudBackupPath) {
     throw new Error("Options `gcloudBackupPath` is mandatory.");
   }
-  if (!options.gcloudServiceAccountKey && !options.gcloudServiceAccountFile) {
-    throw new Error("Options `gcloudServiceAccountKey` or `gcloudServiceAccountFile` is mandatory.");
-  }
 }
